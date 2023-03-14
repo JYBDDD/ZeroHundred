fa417c16a0b4531ea25b0bd64b97349df9ec88d2
JYBDDD <98305534+JYBDDD@users.noreply.github.com>
2023-03-14 20:48:59 +0900

    재업

--- Assets/Resources/Script/Manager/PoolManager.cs
+++ Assets/Resources/Script/Manager/PoolManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PoolManager : IManager
@@ -9,23 +10,82 @@ public class PoolManager : IManager
     public void Push(GameObject pushObject)  // 삭제되는곳에서 직접호출
     {
         pushObject.SetActive(false);
-        poolList.Add(pushObject);
     }
 
-    public GameObject Pop(string objectname,Vector2 position,Quaternion rotation)
+    public GameObject Pop(string objectName,Vector2 position,Quaternion rotation)
     {
-        for (int i = 0; i < poolList.Count; i++)
+        var firstCheck = poolList.Where(_ => _.name.Contains(objectName));
+
+        if (firstCheck.Count() <= 0)
+            return null;
+
+        foreach (var o in firstCheck)
         {
-            if (objectname == poolList[i].gameObject.name && !poolList[i].activeSelf)
+            if (o.activeSelf == false)
             {
-                poolList[i].transform.position = position;
-                poolList[i].transform.rotation = rotation;
-                poolList[i].gameObject.SetActive(true);
-                return poolList[i].gameObject;
+                o.gameObject.SetActive(true);
+                o.transform.SetPositionAndRotation(position, rotation);   // 코드 최적화
+                return o;
             }
         }
         return null;
 
+        /*
+         * 바뀐것
+        객체 풀링을 생성시 삽입정렬하도록 변경
+
+        바꿀것
+        삽입정렬된 객체를 이진탐색해서 찾을 것임 TODO
+
+
+        문제사항
+
+        1. 플레이어 스킬이 안나감
+        2. Bullet이 가끔 안나오고 Null로 뻑날때 있음 (미사일 생길때인거같은데?)
+         */
+    }
+
+    /* // 풀링 요소가 존재하는지 확인
+     public bool PoolingExists(string name,out GameObject outObj)
+     {
+         outObj = null;
+         var firstCheck = poolList.Where(_ => _.name.Contains(name));
+
+         if (firstCheck.Count() <= 0)
+             return false;
+
+         foreach(var o in firstCheck)
+         {
+             if (o.activeSelf == false)
+             {
+                 outObj = o;
+                 return true;
+             }
+         }
+
+         return false;
+     }*/
+
+    // 요소 삽입 정렬
+    public void InsertMerge(GameObject obj)
+    {
+        poolList.Insert(0, obj);
+        //poolList.OrderBy(_ => _.name);
+
+        for (int i = 1; i < poolList.Count; ++i)
+        {
+            int iMinuse = i - 1;
+
+            // 변경시킬 값보다 클경우 종료
+            if (GameManager.Resource.CompareLowCode(poolList[iMinuse].name, poolList[i].name) == true)
+                break;
+            else
+            {
+                GameObject temp = poolList[i];
+                poolList[i] = poolList[iMinuse];
+                poolList[iMinuse] = temp;
+            }
+        }
     }
 
     public void Init()
@@ -40,6 +100,6 @@ public class PoolManager : IManager
 
     public void Clear()
     {
-        
+        poolList.Clear();
     }
 }
--- Assets/Resources/Script/Manager/ResourceManager.cs
+++ Assets/Resources/Script/Manager/ResourceManager.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 public class ResourceManager
 {
+    GameObject[] itemObjects;
+
     public T Load<T>(string path) where T : UnityEngine.Object
     {
         T Path = Resources.Load<T>(path);
@@ -14,106 +18,96 @@ public class ResourceManager
         return Path;
     }
 
+    /// <summary>
+    /// 생성
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="position"></param>
+    /// <param name="rotation"></param>
+    /// <param name="parent"></param>
+    /// <returns></returns>
     public GameObject Instantiate(string path,Vector2 position,Quaternion rotation,Transform parent = null)
     {
+        PoolManager pool = GameManager.Pool;
+        if (parent != null)
+        {
+            for (int i = 0; i < parent.childCount; i++)
+            {
+                Transform trans = parent.GetChild(i);
+                GameObject popItem = pool.Pop(trans.name, position, rotation);
+                if (popItem != null)
+                {
+                    return popItem;
+                }
+            }
+        }
+
+        // 풀링할 객체가 없을경우 로드 및 생성
         GameObject go = Load<GameObject>($"Prefabs/{path}");
 
         if (go == null)
             return null;
 
-        if (parent == GameManager.PlayerBulletParent.transform)                             // Player Weapon 재생성
-        {
-            for (int i = 0; i < GameManager.PlayerBulletParent.transform.childCount; i++)
-            {
-                if (GameManager.PlayerBulletParent.transform.GetChild(i).name.Contains(path) && !GameManager.PlayerBulletParent.transform.GetChild(i).gameObject.activeSelf)
-                {
-                    return GameManager.Pool.Pop(path, position, rotation);
-                }
-            }
-        }
-
-        if (parent == GameManager.EnemyBulletParent.transform)                              // Enemy Weapon 재생성
-        {
-            for (int i = 0; i < GameManager.EnemyBulletParent.transform.childCount; i++)
-            {
-                if (GameManager.EnemyBulletParent.transform.GetChild(i).name.Contains(path) && !GameManager.EnemyBulletParent.transform.GetChild(i).gameObject.activeSelf)
-                {
-                    return GameManager.Pool.Pop(path, position, rotation);
-                }
-            }
-        }
-
-        if (parent == GameManager.MuzzleOfHitParent.transform)                              // Muzzle,HitEffect 재생성
-        {
-            for (int i = 0; i < GameManager.MuzzleOfHitParent.transform.childCount; i++)
-            {
-                if (GameManager.MuzzleOfHitParent.transform.GetChild(i).name.Contains(path) && !GameManager.MuzzleOfHitParent.transform.GetChild(i).gameObject.activeSelf)
-                {
-                    return GameManager.Pool.Pop(path, position, rotation);
-                }
-            }
-        }
-
-        if (parent == GameManager.EnemyObjectParent.transform)                              // Enemy Object 재생성
-        {
-            for (int i = 0; i < GameManager.EnemyObjectParent.transform.childCount; i++)
-            {
-                if (GameManager.EnemyObjectParent.transform.GetChild(i).name.Contains(path) && !GameManager.EnemyObjectParent.transform.GetChild(i).gameObject.activeSelf)
-                {
-                    return GameManager.Pool.Pop(path, position, rotation);
-                }
-            }
-        }
-
-        if (parent == GameManager.DeadEffectParent.transform)                               // DeadEffect 재생성
-        {
-            for (int i = 0; i < GameManager.DeadEffectParent.transform.childCount; i++)
-            {
-                if (GameManager.DeadEffectParent.transform.GetChild(i).name.Contains(path) && !GameManager.DeadEffectParent.transform.GetChild(i).gameObject.activeSelf)
-                {
-                    return GameManager.Pool.Pop(path, position, rotation);
-                }
-            }
-        }
-
-
         GameObject clone = UnityEngine.Object.Instantiate(go, position, rotation, parent);
-        clone.name = path;
+        pool.InsertMerge(clone);        // 삽입 정렬
 
         return clone;
     }
 
     private GameObject ItemsPrefabs()     // 아이템들의 오브젝트를 저장
     {
-        GameObject[] gameObjects = Resources.LoadAll<GameObject>("Prefabs/Item");
+        itemObjects ??= Resources.LoadAll<GameObject>("Prefabs/Item");
 
-        int randNum = Random.Range(0, gameObjects.Length);
+        int randNum = UnityEngine.Random.Range(0, itemObjects.Length);
 
-        return gameObjects[randNum];        // 아이템들중 하나 무작위 추출
+        return itemObjects[randNum];        // 아이템들중 하나 무작위 추출
     }
 
     public GameObject ItemInstantiate(Vector2 position, Quaternion rotation, Transform parent = null)
     {
-        if(Random.Range(0,100) < 10)        // 10% 확률로 생성
+        PoolManager pool = GameManager.Pool;
+
+        if (UnityEngine.Random.Range(0,100) < 10)        // 10% 확률로 생성
         {
+            GameObject dropItem = ItemsPrefabs();
             if (parent == GameManager.ItemObjectParent.transform)                               // ItemObject 재생성
             {
-                for (int i = 0; i < GameManager.ItemObjectParent.transform.childCount; i++)
-                {
-                    if (GameManager.ItemObjectParent.transform.GetChild(i).name.Contains(ItemsPrefabs().name) && !GameManager.ItemObjectParent.transform.GetChild(i).gameObject.activeSelf)
-                    {
-                        return GameManager.Pool.Pop(ItemsPrefabs().name, position, rotation);
-                    }
-                }
+                GameObject popItem = pool.Pop(dropItem.name, position, rotation);
+                if (popItem != null)
+                    return popItem;
             }
 
-            GameObject clone = UnityEngine.Object.Instantiate(ItemsPrefabs(), position, rotation, parent);
-            clone.name = ItemsPrefabs().name;
+            GameObject clone = UnityEngine.Object.Instantiate(dropItem, position, rotation, parent);
+            pool.InsertMerge(clone);        // 삽입 정렬
 
             return clone;
         }
         return null;
     }
 
-    
+    /// <summary>
+    /// 문자열 아스키코드값을 비교하여 compareA값이 작다면 true
+    /// </summary>
+    /// <returns></returns>
+    public bool CompareLowCode(string compareA,string compareB)
+    {
+        byte[] cA = Encoding.ASCII.GetBytes(compareA);
+        byte[] cB = Encoding.ASCII.GetBytes(compareB);
+
+        for(int i = 0; i < cA.Length; ++i)
+        {
+            if (i > cB.Length)
+                return false;
+
+            var codeA = int.Parse(cA[i].ToString());
+            var codeB = int.Parse(cB[i].ToString());
+
+            if (codeA > codeB)
+                return false;
+            else if (codeB > codeA)
+                return true;
+        }
+
+        return false;
+    }
 }
